be77ba4a756b95c9d745ac0de4ebd92f66656785
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-03-25 11:51:51 +0530

    Add Config : [pallavi] - Added Database configuration for use

---
+++ src/Config/database.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+username = os.getenv("username")
+password = quote_plus(os.getenv("password"))
+server = os.getenv("server")
+database = os.getenv("database")
+
+DATABASE_URL = "mssql+pyodbc://@DESKTOP-P8GCKCP\\SQLEXPRESS/FundooAPPdb?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=yes&TrustServerCertificate=yes"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
